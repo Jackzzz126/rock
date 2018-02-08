a274db92ccfcbe10a942d3a5f639e5ca4a7abcc3
Jackzzz126 <Jackzzz@126.com>
2018-02-08 12:20:18 +0800

    Update wsServer to support wss

--- wsServer.js
+++ wsServer.js
@@ -1,8 +1,24 @@
-var WebSocketServer = require('ws').Server;
+var https=require('https');
+var ws=require('ws');
+var fs=require('fs');
 
-function run(port, onConn) {
-	var wss = new WebSocketServer({port: port});
+function run(port, onConn, cerFilePath, keyFilePath) {
+	let wss = null;
+	if(cerFilePath && keyFilePath) {
+		let options = {
+			cert: fs.readFileSync(cerFilePath),
+			key: fs.readFileSync(keyFilePath),
+			//passphrase:'1234'
+		};
+		var server=https.createServer(options, function (req, res) {
+			res.writeHead(403);
+			res.end("This is a WebSockets server!\n");
+		}).listen(port);
 
+		wss = new ws.Server({server: server});
+	} else {
+		wss = new ws.Serve({port: port});
+	}
 	wss.on('connection', function(socket, req) {
 		if(onConn) {
 			onConn(socket, req);
